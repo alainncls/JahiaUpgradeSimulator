71f699c31f1747f6c4a6439ecd3078837140dc05
alainncls <alain.nicolas@epfedu.fr>
2015-02-05 09:53:43 +0100

    Et là ?

--- src/fr/smile/fiches/fAccueil.java
+++ src/fr/smile/fiches/fAccueil.java
@@ -204,14 +204,6 @@ public class fAccueil extends JFrame {
 		bPatches.setVisible(false);
 		contentPane.add(bPatches);
 
-		/*
-		 * Quick init for debug
-		 */
-		cbStart.setSelectedItem("6.6.2.7");
-		cbEnd.setSelectedItem("7.0.0.4");
-		rbStandalone.doClick();
-		bSimulate.doClick();
-		bPatches.doClick();
 
 	}
 
