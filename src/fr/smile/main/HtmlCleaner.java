efc7bd3f55714bdc3736e6b437dc45b0693476e6
alainncls <alain.nicolas@epfedu.fr>
2015-02-20 15:54:25 +0100

    [FEATURE] Ajout HtmlCleaner

---
+++ src/fr/smile/main/HtmlCleaner.java
@@ -0,0 +1,9 @@
+package fr.smile.main;
+
+public class HtmlCleaner {
+
+	public String warningCleaner(String toClean) {
+		return toClean.replaceAll("<li>", "\n\t- ").replaceAll("<br/>", "\n")
+				.replaceAll("\\<.*?>", "");
+	}
+}
